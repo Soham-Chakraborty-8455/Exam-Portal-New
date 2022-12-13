6e42a9c4d003f0a620ed88e51f8877859d75e9f9
Soham Chakraborty <schakraborty8455@gmail.com>
2022-12-13 20:29:08 +0530

    STUDENT DB

---
+++ students.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///student.db"
+db.init_app(app)
+
+class Students(db.Model):
+    enrollment_number = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    phone_number = db.Column(db.Integer, unique=True, nullable=False)
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
