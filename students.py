f8ecc05ac2aea1d490c5b8f15466fc3c5510a94b
Soham Chakraborty <schakraborty8455@gmail.com>
2022-12-20 01:27:53 +0530

    Updated

--- students.py
+++ students.py
@@ -1,15 +1,60 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
 
+#QUESTION PART
+#------------------------------------------
 
-db = SQLAlchemy()
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb+srv://IEMITDEPT:DepartmentofIT@cluster0.exh0dky.mongodb.net/?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+questions = mongo.db.questions
+@app.route('/add', methods=['POST'])
+def add_todo():
+    ExamName = request.form.get('ExamName')
+    SubjectCode = request.form.get('SubjectCode')
+    Session = request.form.get('Session')
+    Date = request.form.get('Date')
+    StartTime = request.form.get('StartTime')
+    EndTime = request.form.get('EndTime')
+    QuestionValue = request.form.get('QuestionValue')
+    Option1 = request.form.get('Option1')
+    Option2 = request.form.get('Option2')
+    Option3 = request.form.get('Option3')
+    Option4 = request.form.get('Option4')
+    CorrectMarks = request.form.get('CorrectMarks')
+    CorrectAnswer = request.form.get('CorrectAnswer')
+    NegativeMarks = request.form.get('NegativeMarks')
+    questions.insert_one({'ExamName':ExamName,
+        'SubjectCode':SubjectCode,
+        'Session':Session,
+        'Date':Date,
+        'StartTime':StartTime,
+        'EndTime':EndTime,
+        'QuestionData': [
+            {
+                'Question': QuestionValue,
+                'Options':[{'1':Option1,'2':Option2,'3':Option3,'4':Option4}],
+                'CorrectMarks':CorrectMarks,
+                'CorrectAnswer':CorrectAnswer,
+                'NegativeMarks':NegativeMarks,
+            },
+        ]
+})
+    return redirect(render_template(questions.html))
+
+
+
+
+
+
+
+#STUDENT RECORD PART
+#-------------------------------------------------------------------
+db = SQLAlchemy()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///student.db"
 db.init_app(app)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-mongo = PyMongo(app)
-
 class Students(db.Model):
     enrollment_number = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
