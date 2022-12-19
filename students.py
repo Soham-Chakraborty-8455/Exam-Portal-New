acfa0f40b243c1c6b1bbd998adef22ab90366a19
Soham Chakraborty <schakraborty8455@gmail.com>
2022-12-19 23:37:23 +0530

    Updated

--- students.py
+++ students.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///student.db"
 db.init_app(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
 class Students(db.Model):
     enrollment_number = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.Integer, unique=True, nullable=False)
-    marks = db.Column(db.Integer, nullable=False)
 
 with app.app_context():
     db.create_all()
